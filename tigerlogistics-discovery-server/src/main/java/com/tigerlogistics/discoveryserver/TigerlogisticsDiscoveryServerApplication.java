b8d53144d6139bb0f5658726335c0092d81cccae
RajasreePK <pkrajasree.career@gmail.com>
2022-02-16 14:58:46 +0530

    new sub folder created

---
+++ tigerlogistics-discovery-server/src/main/java/com/tigerlogistics/discoveryserver/TigerlogisticsDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.tigerlogistics.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigerlogisticsDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TigerlogisticsDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ tigerlogistics-discovery-server/src/test/java/com/tigerlogistics/discoveryserver/TigerlogisticsDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tigerlogistics.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TigerlogisticsDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
