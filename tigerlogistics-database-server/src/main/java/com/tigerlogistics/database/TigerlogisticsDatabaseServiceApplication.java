cdb387aa8e72bb4aafc11b4a39ffe2f48ad3bcff
akhil murali <akhilmurali662@gmail.com>
2022-03-07 18:04:39 +0530

    addeddiscovery

---
+++ tigerlogistics-database-server/src/main/java/com/tigerlogistics/database/TigerlogisticsDatabaseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tigerlogistics.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigerlogisticsDatabaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TigerlogisticsDatabaseServiceApplication.class, args);
+	}
+
+}
---
+++ tigerlogistics-database-server/src/test/java/com/tigerlogistics/database/TigerlogisticsDatabaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tigerlogistics.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TigerlogisticsDatabaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
