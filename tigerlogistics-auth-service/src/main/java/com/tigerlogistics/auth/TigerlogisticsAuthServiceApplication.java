bcb258dfbfc10dd646e2d52766a52f22058d3b5a
akhil murali <akhilmurali662@gmail.com>
2022-03-11 16:56:37 +0530

    addedauth

---
+++ tigerlogistics-auth-service/src/main/java/com/tigerlogistics/auth/TigerlogisticsAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tigerlogistics.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigerlogisticsAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TigerlogisticsAuthServiceApplication.class, args);
+	}
+
+}
---
+++ tigerlogistics-rawmaterial-order-service/src/main/java/com/tigerlogistics/rawmaterial/order/TigerlogisticsRawmaterialOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tigerlogistics.rawmaterial.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigerlogisticsRawmaterialOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TigerlogisticsRawmaterialOrderServiceApplication.class, args);
+	}
+
+}
