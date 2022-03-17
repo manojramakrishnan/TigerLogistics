8aebb98604998d2797be111fe4d73937d7c1189a
akhil murali <akhilmurali662@gmail.com>
2022-03-17 15:47:18 +0530

    changepassword

--- logistics-frontend/src/app/auth/request-question/request-question.component.ts
+++ logistics-frontend/src/app/auth/request-question/request-question.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+import { EventService } from 'src/app/services/event.service';
+import { LoadingService } from 'src/app/services/loading.service';
 
 @Component({
   selector: 'app-request-question',
   templateUrl: './request-question.component.html',
-  styleUrls: ['./request-question.component.css']
+  styleUrls: ['./request-question.component.css'],
 })
-export class RequestQuestionComponent implements OnInit {
+export class RequestQuestionComponent implements OnInit, OnDestroy {
+  requestQuestionForm: FormGroup;
+  formSubscription: Subscription;
+  submitted = false;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    public loadingService: LoadingService,
+    private eventService: EventService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnDestroy(): void {
+    if (this.formSubscription) this.formSubscription.unsubscribe();
   }
 
+  ngOnInit(): void {
+    this.initQuestionForm();
+  }
+
+  requestSecretQuestion() {
+    this.submitted = true;
+    if (this.requestQuestionForm.valid)
+      this.submitData(this.requestQuestionForm.value.username);
+  }
+
+  submitData(formData) {
+    this.loadingService.enableLoading();
+    this.formSubscription = this.authService
+      .requestSecretQuestion(formData)
+      .subscribe(
+        (response) => {
+          this.loadingService.disableLoading();
+          this.eventService.resetPasswordData.next(response);
+          this.router.navigateByUrl('/login/changepassword');
+        },
+        (error) => {
+          this.loadingService.disableLoading();
+          if (error.error.message === 'FieldException')
+            error.error.errors.forEach((element) =>
+              this.requestQuestionForm.controls[element.field]?.setErrors({
+                serverValidationError: element.message,
+              })
+            );
+          else throw new Error(error);
+        }
+      );
+  }
+
+  initQuestionForm() {
+    this.requestQuestionForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+    });
+  }
 }
