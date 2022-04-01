9011ed1ddcee0f17cf97ac9917f34ace6fab89a0
akhil murali <akhilmurali662@gmail.com>
2022-04-01 15:56:29 +0530

    authservice

--- logistics-frontend/src/app/services/auth.service.ts
+++ logistics-frontend/src/app/services/auth.service.ts
@@ -1,17 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/common/http';
-import { Environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment';
 import { User } from '../models/user.model';
-import { Tap } from 'rsjs/operators';
+import { tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
-import { JwtHelperService } from '@authzero/angular-jwt';
-import { EventService } from './event.service';
-
-
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { EventService } from '.event.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
+
 })
+
 export class AuthService {
   authServiceUrl = `${environment.protocol}${environment.applicationUrl}/${environment.authService}`;
 
@@ -19,49 +19,50 @@ export class AuthService {
     private http: HttpClient,
     private router: Router,
     private jwtHelper: JwtHelperService,
-    private eventService: EventService,
+    private eventService: EventService
+  ) {}
 
-  ) { }
+  login(formData) {
+    return this.http
+      .post(`${this.authServiceUrl}/auth/login`, formdata)
+      .pipe(tap((user: User) => {
+        this.saveToSessionStorage(user);
+      }));
+  }
 
-  login(formData){}
-  return this.http.post(`${this.authServiceUrl}/auth/login`,formData)
-  .pipe(tap((user: User) => {
-    this.saveToSessionStorage(user);
-    this.eventService.loggedInUser.next(user);
-
-  }));
   saveToSessionStorage(user: User) {
-    sessionStorage.setItem('user', Jason.stringify(user));
-}
+    sessionStorage.setItem('user' , JSON.stringify(user));
+  }
 
-fetchFromSessionStorage(): User{
-  return JASON.parse(sessionStorage.getItem('user'));
-}
+  fetchFromSessionStorage(): User {
+    return JSON.parse(sessionStorage.getItem('user'));
+  }
 
-logOut(): Void {
-  sessionStorage.clear();
-  localStorage.clear();
-  this.router.navigate(['/login']);
-}
+  logout(): void {
+    sessionStorage.clear();
+    localStorage.clear();
+    this.router.navigate(['/login']);
+  }
 
-redirectIfLoggedIn(){
-  if (this.fetchFromSessionStorage()?.token)
-  this.router.navigate(['/dashboard']);
-}
+  redirectIfLoggedIn() {
+    if (this.fetchFromSessionStorage()?.token)
+      this.router.navigate(['/dashboard']);
+  }
 
-isAuthenticated(): Boolean {
-  const token = this.fetchFromSessionStorage()?.token;
-  retun !this.jwtHelper.isTokenExpired(token);
-}
-requestSecretQuestion(username){
-  return this.http.get(`${this.authServiceUrl}/auth/forgetpassword/${username}`);
-}
-requestPasswordReset(formData) {
-  return this.http.put(`${this.authServiceUrl}/auth/forgetpassword`, formaData);
-}
-getRole(){
-  return this.fetchFromSessionStorage()?.role;
-  
-}
+  isAuthenticated(): boolean {
+    const token = this.fetchFromSessionStorage()?.token;
+    return !this.jwtHelper.isTokenExpired(token);
+  }
 
+  requestSecretQuestion (username) {
+    return this.http.get(`${this.authServiceUrl}/auth/forgotpassword/${username}`);
+  }
+
+  requestPasswordReset (formData) {
+    return this.http.put(`${this.authServiceUrl}/auth/forgotpassword`, formData);
+  }
+
+  getRole() {
+    return this.fetchFromSessionStorage()?.role;
+  }
 }
