8e28d41c61b76544e59e3da239c333f320d91dd0
akhil murali <akhilmurali662@gmail.com>
2022-04-05 16:07:14 +0530

    productservice

---
+++ logistics-frontend/src/app/services/product.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  serviceUrl = `${environment.protocol}${environment.applicationUrl}/${environment.stockManagementService}`;
+
+  constructor(private http: HttpClient) {}
+
+  fetchAllProducts() {
+    return this.http.get(this.serviceUrl + '/productStock');
+  }
+
+  getProductByProductId(productId: number) {
+      return this.http.get(this.serviceUrl + '/productStock/' + productId);
+  }
+
+  addProduct(formData) {
+      return this.http.post(this.serviceUrl + '/productStock', formData);
+  }
+}
