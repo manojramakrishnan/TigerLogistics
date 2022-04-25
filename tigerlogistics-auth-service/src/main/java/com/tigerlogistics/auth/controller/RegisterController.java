9f4f8f4ae25254f0f94f1cb1b316016e4afb7826
Naveen <naveen25navics@gmail.com>
2022-04-25 16:09:57 +0530

    modified mapping

--- tigerlogistics-auth-service/src/main/java/com/tigerlogistics/auth/controller/RegisterController.java
+++ tigerlogistics-auth-service/src/main/java/com/tigerlogistics/auth/controller/RegisterController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +24,7 @@ import com.tigerlogistics.auth.service.AuthService;
 public class RegisterController {
 	@Autowired
 	private AuthService authService;
-	@PostMapping
+	@PutMapping
 	public ResponseEntity<UserDetailsDTO> register(@RequestBody RegisterRequest registerRequest){
 	
 			return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(registerRequest));
