81185e9b8eeab90a53d3f1436cc88e3e0eb2dcd1
akhil murali <akhilmurali662@gmail.com>
2022-04-27 15:39:01 +0530

    productorderservice

---
+++ tigerlogistics-product-order-service/src/main/java/com/tigerlogistics/productorder/controller/ProductOrderController.java
@@ -0,0 +1,5 @@
+package com.tigerlogistics.productorder.controller;
+
+public class ProductOrderController {
+
+}
---
+++ tigerlogistics-product-order-service/src/main/java/com/tigerlogistics/productorder/service/impl/ProductOrderServiceImpl.java
@@ -0,0 +1,8 @@
+package com.tigerlogistics.productorder.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductOrderServiceImpl {
+
+}
