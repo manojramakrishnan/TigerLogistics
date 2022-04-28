91df7971707e6667160f13aa753f7274ddf6b7c1
manojramakrishnan <manoj.rgv@gmail.com>
2022-04-28 23:15:24 +0530

    updated the fields and removed unwanted imports and added required imports.

--- tigerlogistics-product-order-service/src/main/java/com/tigerlogistics/productorder/entity/ProductOrder.java
+++ tigerlogistics-product-order-service/src/main/java/com/tigerlogistics/productorder/entity/ProductOrder.java
@@ -1,39 +1,50 @@
 package com.tigerlogistics.productorder.entity;
 
-import static javax.persistence.GenerationType.SEQUENCE;
+
 
 import java.time.LocalDate;
-
+import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
-
+import javax.persistence.GenerationType;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ForeignKey;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import java.time.LocalDate;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import com.tigerlogistics.productorder.enums.OrderStatus;
+import com.tigerlogistics.productorder.enums.QualityCheck;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
+@Data
 public class ProductOrder {
 	
 	@Id
 	@SequenceGenerator(name="product_order_id_sequence",initialValue=10000,allocationSize=1)
-	@GeneratedValue(generator="product_order_id_sequence",strategy=SEQUENCE)
+	@GeneratedValue(generator="product_order_id_sequence",strategy=GenerationType.SEQUENCE)
 	private Long productOrderId;
 	private Double quantity;
 	private Double pricePerUnit;
+	@Enumerated(EnumType.STRING)
 	private QualityCheck qualityCheck;
+	@Enumerated(EnumType.STRING)
 	private OrderStatus orderStatus;
 	private LocalDate deliveryDate;
 	private LocalDate manufactureDate;
 	private LocalDate expiryDate;
 	private LocalDate orderedOn;
+	@ManyToOne
+  	@JoinColumn(name = "distributorId", referencedColumnName = "distributorId", foreignKey = @ForeignKey(name = "FK_product_distributor_ID"))
 	private Distributor distributor;
+	@ManyToOne
+  	@JoinColumn(name = "productId", referencedColumnName = "productId", foreignKey = @ForeignKey(name = "FK_product_order_ID"))
 	private Product product;
 
 }
