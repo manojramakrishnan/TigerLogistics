4477bb40a938283a8cbf167c80ee8fd55bed575a
manojramakrishnan <manoj.rgv@gmail.com>
2022-04-28 22:45:34 +0530

    removed unwanted annotations and added required annotation

--- tigerlogistics-product-order-service/src/main/java/com/tigerlogistics/productorder/entity/Distributor.java
+++ tigerlogistics-product-order-service/src/main/java/com/tigerlogistics/productorder/entity/Distributor.java
@@ -9,15 +9,14 @@ import javax.persistence.SequenceGenerator;
 
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
+@Data
 public class Distributor {
 
 	@Id
