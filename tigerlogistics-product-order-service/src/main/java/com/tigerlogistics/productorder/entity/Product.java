68269e6272ac2a3f3e7fe110be8db19f92ba8c02
manojramakrishnan <manoj.rgv@gmail.com>
2022-04-28 23:01:21 +0530

    added all the imports correctly

--- tigerlogistics-product-order-service/src/main/java/com/tigerlogistics/productorder/entity/Product.java
+++ tigerlogistics-product-order-service/src/main/java/com/tigerlogistics/productorder/entity/Product.java
@@ -1,32 +1,39 @@
 package com.tigerlogistics.productorder.entity;
 
-import static javax.persistence.GenerationType.SEQUENCE;
+
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
-
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import javax.persistence.ForeignKey;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
+import com.tigerlogistics.productorder.enums.MeasurementUnit;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
+@Data
 public class Product {
 
 	@Id
 	@SequenceGenerator(name="product_id_sequence",initialValue=10000,allocationSize=1)
-	@GeneratedValue(generator="product_id_sequence",strategy=SEQUENCE)
+	@GeneratedValue(generator="product_id_sequence",strategy=GenerationType.SEQUENCE)
 	private Long productId;
 	private String materialName;
 	private String description;
 	private Double quatityAvailable;
+	@Enumerated(EnumType.STRING)
 	private MeasurementUnit quantityUnit;
+	@ManyToOne
+  	@JoinColumn(name = "warehouseId", referencedColumnName = "warehouseId", foreignKey = @ForeignKey(name = "FK_prod_warehouse_ID"))
 	private Warehouse warehouse;
 	
 }
