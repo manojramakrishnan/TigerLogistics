fb8ad97ab257e9634b354255d77dc1757e859222
akhil murali <akhilmurali662@gmail.com>
2022-05-04 15:54:02 +0530

    getproductbyid

--- tigerlogistics-product-order-service/src/main/java/com/tigerlogistics/productorder/controller/ProductOrderController.java
+++ tigerlogistics-product-order-service/src/main/java/com/tigerlogistics/productorder/controller/ProductOrderController.java
@@ -8,12 +8,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tigerlogistics.productorder.dto.ProductDetailsDTO;
 import com.tigerlogistics.productorder.dto.ProductOrderRequest;
 import com.tigerlogistics.productorder.dto.ProductOrderResponse;
 import com.tigerlogistics.productorder.service.ProductOrderService;
@@ -39,4 +39,8 @@ public class ProductOrderController {
     	
     }
 
+	@GetMapping("/fetchproductbyid/{productOrderId}")
+	public ResponseEntity<ProductOrderResponse> fetchProductsById(@PathVariable  long productOrderId){
+		return ResponseEntity.status(HttpStatus.OK).body(this.productOrderService.fetchProductById(productOrderId));
+	}
 }
--- tigerlogistics-product-order-service/src/main/java/com/tigerlogistics/productorder/repository/ProductOrderRepository.java
+++ tigerlogistics-product-order-service/src/main/java/com/tigerlogistics/productorder/repository/ProductOrderRepository.java
@@ -3,12 +3,16 @@ package com.tigerlogistics.productorder.repository;
 import java.util.Map;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.tigerlogistics.productorder.entity.ProductOrder;
 
 @Repository
 public interface ProductOrderRepository extends JpaRepository<ProductOrder,Long> {
+
+	@Query("SELECT p FROM ProductOrder p where p.id = :productOrderId")
+	ProductOrder findProductById(long productOrderId);
 
 
 }
