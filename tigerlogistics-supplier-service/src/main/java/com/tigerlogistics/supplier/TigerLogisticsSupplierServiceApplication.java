adff98cfca26b717a1cc1fe59ab6232130ef0996
Naveen <naveen25navics@gmail.com>
2022-05-16 12:22:58 +0530

    supplier service class and interface added

--- tigerlogistics-supplier-service/src/main/java/com/tigerlogistics/supplier/TigerLogisticsSupplierServiceApplication.java
+++ tigerlogistics-supplier-service/src/main/java/com/tigerlogistics/supplier/TigerLogisticsSupplierServiceApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.tigerlogistics.supplier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TigerLogisticsSupplierServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TigerLogisticsSupplierServiceApplication.class, args);
 	}
 
 }
--- tigerlogistics-supplier-service/src/test/java/com/tigerlogistics/supplier/TigerLogisticsSupplierServiceApplicationTests.java
+++ tigerlogistics-supplier-service/src/test/java/com/tigerlogistics/supplier/TigerLogisticsSupplierServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.tigerlogistics.supplier;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class TigerLogisticsSupplierServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
