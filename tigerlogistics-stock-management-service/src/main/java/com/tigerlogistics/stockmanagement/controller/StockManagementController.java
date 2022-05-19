a1e140ea4ab5308d7e9b8d4ab71550f4adbec073
akhil murali <akhilmurali662@gmail.com>
2022-05-19 19:12:25 +0530

    added code for create product for api

--- tigerlogistics-stock-management-service/src/main/java/com/tigerlogistics/stockmanagement/controller/StockManagementController.java
+++ tigerlogistics-stock-management-service/src/main/java/com/tigerlogistics/stockmanagement/controller/StockManagementController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tigerlogistics.stockmanagement.dto.ProductAddRequest;
 import com.tigerlogistics.stockmanagement.entity.Product;
 import com.tigerlogistics.stockmanagement.service.StockManagementService;
 
--- tigerlogistics-stock-management-service/src/main/java/com/tigerlogistics/stockmanagement/service/StockManagementService.java
+++ tigerlogistics-stock-management-service/src/main/java/com/tigerlogistics/stockmanagement/service/StockManagementService.java
@@ -2,7 +2,6 @@ package com.tigerlogistics.stockmanagement.service;
 
 import java.util.Map;
 
-import com.tigerlogistics.stockmanagement.dto.ProductAddRequest;
 import com.tigerlogistics.stockmanagement.entity.Product;
 
 public interface StockManagementService {
