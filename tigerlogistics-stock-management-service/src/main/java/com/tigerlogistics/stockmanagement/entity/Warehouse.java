2a92d8ec6dadba92d4bae7eb1710aac3cf07f3a9
akhil murali <akhilmurali662@gmail.com>
2022-05-23 17:03:33 +0530

    added application property

--- tigerlogistics-stock-management-service/src/main/java/com/tigerlogistics/stockmanagement/entity/Warehouse.java
+++ tigerlogistics-stock-management-service/src/main/java/com/tigerlogistics/stockmanagement/entity/Warehouse.java
@@ -1,5 +1,27 @@
 package com.tigerlogistics.stockmanagement.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Warehouse {
+
+	@Id
+	@SequenceGenerator(name="warehouse_id_sequence",initialValue=10000,allocationSize=1)
+	@GeneratedValue(generator="warehouse_id_sequence",strategy=GenerationType.SEQUENCE)
+	private Long warehouseId;
+	private String name;
+	private String description;
 
 }
