5131ccc08865aff4284f7db11a5190cffc02abbd
akhil murali <akhilmurali662@gmail.com>
2022-06-21 15:31:54 +0530

    added get distributor by id api

--- tigerlogistics-supplier-service/src/main/java/com/tigerlogistics/supplier/repository/DistributorRepository.java
+++ tigerlogistics-supplier-service/src/main/java/com/tigerlogistics/supplier/repository/DistributorRepository.java
@@ -1,11 +1,15 @@
 package com.tigerlogistics.supplier.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.tigerlogistics.supplier.entity.Distributor;
 
 @Repository
 public interface DistributorRepository extends JpaRepository<Distributor,Long>{
+
+	@Query("SELECT d FROM Distributor d where d.id = :distributorId")
+	Distributor findDistributorById(long distributorId);
 
 }
