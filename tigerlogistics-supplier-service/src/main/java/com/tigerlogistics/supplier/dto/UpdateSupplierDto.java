16a68cab34c2beac1e8868e6447befe9d3164016
Naveen <naveen25navics@gmail.com>
2022-06-30 13:09:40 +0530

    Smallchanges

--- tigerlogistics-supplier-service/src/main/java/com/tigerlogistics/supplier/dto/UpdateSupplierDto.java
+++ tigerlogistics-supplier-service/src/main/java/com/tigerlogistics/supplier/dto/UpdateSupplierDto.java
@@ -5,12 +5,16 @@ import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Getter
+@Setter
 public class UpdateSupplierDto {
 	@NotNull
 	private Long supplierId;
