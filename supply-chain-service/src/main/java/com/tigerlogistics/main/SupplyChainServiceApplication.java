32e20b5e6f154c09803365b660fd6e7da61951b8
akhil murali <akhilmurali662@gmail.com>
2022-07-13 15:17:58 +0530

    added supplier chain service file

---
+++ supply-chain-service/src/main/java/com/tigerlogistics/main/SupplyChainServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tigerlogistics.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyChainServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyChainServiceApplication.class, args);
+	}
+
+}
---
+++ supply-chain-service/src/test/java/com/tigerlogistics/main/SupplyChainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tigerlogistics.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyChainServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
