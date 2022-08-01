f0402d73da6a2c99827f3be5ad0992fd5ff21674
Naveen <naveen25navics@gmail.com>
2022-08-01 12:04:14 +0530

    find All added in supplier chain service

--- supply-chain-service/src/main/java/com/tigerlogistics/main/controller/ReadOnlyRestController.java
+++ supply-chain-service/src/main/java/com/tigerlogistics/main/controller/ReadOnlyRestController.java
@@ -1,11 +1,13 @@
 package com.tigerlogistics.main.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.tigerlogistics.main.dto.GlobalResponseDto;
 import com.tigerlogistics.main.model.Model;
 import com.tigerlogistics.main.service.SupplyChainReadOnlyService;
 
@@ -25,6 +27,7 @@ public class ReadOnlyRestController<T extends Model<U>,U extends Serializable> {
 			T entity=supplyChainReadOnlyService.getById(id);
 			if (entity == null) {
 				response=new GlobalResponseDto<>("entity not found with id"+ id);
+			}
 				else {
 					response=new GlobalResponseDto<>(entity);
 				}
@@ -35,6 +38,25 @@ public class ReadOnlyRestController<T extends Model<U>,U extends Serializable> {
 			}
 			return response;
 		}
+	@RequestMapping(method=RequestMethod.GET,produces="application/json; charset=UTF-8")
+	public GlobalResponseDto<T> getAll(){
+		GlobalResponseDto<T> response=new GlobalResponseDto<>("unable to obtain all rcords from db");
+		try {
+			List<T> entities=supplyChainReadOnlyService.getAll();
+			if ((entities == null)||(entities.isEmpty())) {
+				response=new GlobalResponseDto<>("entities not found in db");
+			}
+				else {
+					response=new GlobalResponseDto<>(entities);
+				}
+			}
+			catch(Exception e){
+				response=new GlobalResponseDto<>(e.getMessage());
+				
+			}
+			return response;
+		
 	}
 	
-}
+	}
+	
--- supply-chain-service/src/main/java/com/tigerlogistics/main/repository/HibernateReadOnlyRepository.java
+++ supply-chain-service/src/main/java/com/tigerlogistics/main/repository/HibernateReadOnlyRepository.java
@@ -1,5 +1,30 @@
 package com.tigerlogistics.main.repository;
 
-public abstract class HibernateReadOnlyRepository {
+import java.io.Serializable;
+import java.util.List;
 
+import org.hibernate.SessionFactory;
+import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
+
+import com.tigerlogistics.main.model.Model;
+
+public abstract class HibernateReadOnlyRepository<T extends Model<U>, U extends Serializable>
+extends HibernateDaoSupport implements ReadOnlyRepository<T, U> {
+private Class<T> type;
+
+public HibernateReadOnlyRepository(Class<T> type, SessionFactory sessionFactory) {
+this.type = type;
+setSessionFactory(sessionFactory);
+}
+
+@Override
+public T getById(U id) {
+if(id == null) return null;
+return getHibernateTemplate().get(type, id);
+}
+
+@Override
+public List<T> getAll() {
+return getHibernateTemplate().loadAll(type);
+}
 }
